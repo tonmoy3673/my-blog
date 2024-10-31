8723a9f02255a53f38bec205aa5a8d4d2bb6156d
tonmoy <tonmoy3673@gmail.com>
2024-10-31 21:37:01 +0600

    :bulb: display users based on length

--- src/Component/Pages/Users.jsx
+++ src/Component/Pages/Users.jsx
@@ -17,7 +17,7 @@ const Users = () => {
             <h2 className="text-center text-2xl font-semibold">Users Section</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-10 py-4 mt-4">
                 {
-                    users && users.map((user)=><User user={user} key={user.id}></User>)
+                    usersView && usersView.slice(0,usersLength).map((user)=><User user={user} key={user.id}></User>)
                 }
             </div>
         </div>
